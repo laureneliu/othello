14ae94350400463cafa17c3b89791f08c06d4d48
laureneliu <17laurenl@gmail.com>
2018-03-02 14:24:27 -0800

    ...

--- player.cpp
+++ player.cpp
@@ -12,12 +12,12 @@ Player::Player(Side side) {
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
-     * 30 seconds.
+     * 30 seconds
      */
 }
 
 /*
- * Destructor for the player.
+ * Destructor for the player
  */
 Player::~Player() {
 }
